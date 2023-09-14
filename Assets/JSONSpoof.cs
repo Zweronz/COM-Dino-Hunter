ddcc41c34c656c66aab547ed94b0d52307162eb4
EnderPurrson <enderlord9990@gmail.com>
2023-09-13 22:21:41 -0400

    asdas

--- Assets/JSONSpoof.cs
+++ Assets/JSONSpoof.cs
@@ -50,11 +50,61 @@ public class JSONSpoofValue
 
 	public static implicit operator double(JSONSpoofValue value)
 	{
+		if (PlayerPrefs.GetString(value.key) == "")
+		{
+			return 0;
+		}
+
 		return double.Parse(PlayerPrefs.GetString(value.key));
 	}
 
 	public static implicit operator bool(JSONSpoofValue value)
 	{
 		return PlayerPrefs.GetString(value.key) == "true";
+	}
+
+	private static JSONSpoofArray ParseArray(JSONSpoofValue spoofValue, string key)
+	{
+		JSONSpoofArray value = new JSONSpoofArray(key + "_" + spoofValue.key);
+		for (int i = 0;; i++)
+		{
+			string stringValue = PlayerPrefs.GetString(spoofValue.key + "_" + i);
+			if (stringValue == "")
+			{
+				break;
+			}
+			value.Add(new JSONSpoofValue(stringValue, new JSONSpoof("object_" + key + "_" + spoofValue.key)));
+		}
+		return value;
+	}
+}
+
+public class JSONSpoofArray
+{
+	public List<JSONSpoofValue> values;
+
+	private string extraKey;
+
+	public JSONSpoofArray(string extraKey)
+	{
+		values = new List<JSONSpoofValue>();
+		this.extraKey = extraKey;
+	}
+
+	public JSONSpoofValue this[int index]
+	{
+		get
+		{
+			return values[index];
+		}
+		set
+		{
+			values[index] = value;
+		}
+	}
+
+	public void Add(JSONSpoofValue value)
+	{
+		values.Add(value);
 	}
 }
